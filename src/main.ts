6836e897854dbe457f48be65173634b3eac6c58e
jkoki91 <>
2023-06-22 18:54:23 +0200

    nest auth backend finished

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+  
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -13,6 +15,6 @@ async function bootstrap() {
     })
   );
 
-  await app.listen(3000);
+  await app.listen( process.env.PORT ?? 3000 );
 }
 bootstrap();
